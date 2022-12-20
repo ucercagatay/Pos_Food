450c251bb1a7fc980211165c70e2a82d8f082edc
Tuncay FORMA <tuncayforma@hotmail.com>
2022-12-20 19:41:36 +0300

    yeni tasarım

--- resources/views/sale_pos/receipts/packing_slip_multi.blade.php
+++ resources/views/sale_pos/receipts/packing_slip_multi.blade.php
@@ -1,248 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<title>Document</title>
+	<style>
+		*{
+			box-sizing: border-box;
+		}
+	</style>
+</head>
+<body style="font-family: 'Poppins', sans-serif;padding: 0;margin: 0;">
+<h1 style="text-align:center">
 
-<table style="width:100%;">
-	<thead>
-		<tr>
-			<td>
+	<img src="./public/img/slip_logo.png" alt="" style="margin: auto; display:block;width: 261px;">
 
-			<p class="text-right color-555">
-				@lang('lang_v1.packing_slip')
-			</p>
 
-			</td>
-		</tr>
-	</thead>
-
-	<tbody>
-		<tr>
-			<td>
-
-<!-- business information here -->
-<div class="row invoice-info">
-
-	<div class="col-md-6 invoice-col width-50 color-555">
-		
-		<!-- Logo -->
-		@if(!empty($receipts[0]->logo))
-			<img style="max-height: 120px; width: auto;" src="{{$receipts[0]->logo}}" class="img">
-			<br/>
-		@endif
-
-		<!-- Shop & Location Name  -->
-		@if(!empty($receipts[0]->display_name))
-			<p>
-				{{$receipts[0]->display_name}}
-				@if(!empty($receipts[0]->address))
-					<br/>{!! $receipts[0]->address !!}
-				@endif
-
-				@if(!empty($receipts[0]->contact))
-					<br/>{!! $receipts[0]->contact !!}
-				@endif
-
-				@if(!empty($receipts[0]->website))
-					<br/>{{ $receipts[0]->website }}
-				@endif
-
-				@if(!empty($receipts[0]->tax_info1))
-					<br/>{{ $receipts[0]->tax_label1 }} {{ $receipts[0]->tax_info1 }}
-				@endif
-
-				@if(!empty($receipts[0]->tax_info2))
-					<br/>{{ $receipts[0]->tax_label2 }} {{ $receipts[0]->tax_info2 }}
-				@endif
-
-				@if(!empty($receipts[0]->location_custom_fields))
-					<br/>{{ $receipts[0]->location_custom_fields }}
-				@endif
-			</p>
-		@endif
+</h1>
+<div style="display: flex;">
+	<div style="width: 48%; display:inline-block;">
+		<p style="font-weight: 600;text-decoration: underline;">Sun Früchte Fbr, Inh.Selcuk&Volkan Günex, Robert-Bunsen-str.7.79108 Freiburg</p>
+		<h3 style="margin-bottom: 61px;">BAYDAR S</h3>
+		<h3>STEINGASSE 1</h3>
+		<table>
+			<tr>
+				<td style="width:20%"><h4 style="margin: 0;">79227</h4></td>
+				<td style="width:50%"><h4 style="margin: 0;">SCHALLSTADT</h4></td>
+				<td style="width:30%"><h4 style="margin: 0;">Deutschland</h4></td>
+			</tr>
+		</table>
 	</div>
-
-	<div class="col-md-6 invoice-col width-50">
-
-		<p class="text-right font-30">
-			@if(!empty($receipts[0]->invoice_no_prefix))
-				<span class="pull-left">{!! $receipts[0]->invoice_no_prefix !!}</span>
-			@endif
-			@foreach($receipts as $receipt_details)
-
-				{{$receipt_details->invoice_no}},
-			@endforeach
-		</p>
-		<!-- Date-->
-		@if(!empty($receipts[0]->date_label))
-			<p class="text-right font-23 color-555">
-				<span class="pull-left">
-					{{$receipts[0]->date_label}}
-				</span>
-
-				{{collect($receipts)->sortBy('invoice_date')->reverse()->toArray()[0]->invoice_date}}-{{collect($receipts)->sortBy('invoice_date')->reverse()->toArray()[count($receipts)-1]->invoice_date}}
-			</p>
-		@endif
-	</div>
-</div>
-
-<div class="row invoice-info color-555">
-	<br/>
-	<div class="col-md-6 invoice-col width-50 word-wrap">
-		@if(!empty($receipts[0]->customer_label))
-			<b>{{ $receipts[0]->customer_label }}</b><br/>
-		@endif
-
-		<!-- customer info -->
-		@if(!empty($receipts[0]->customer_name))
-			{{ $receipts[0]->customer_name }}<br>
-		@endif
-		@if(!empty($receipts[0]->customer_info))
-			{!! $receipts[0]->customer_info !!}
-		@endif
-		@if(!empty($receipts[0]->client_id_label))
-			<br/>
-			<strong>{{ $receipts[0]->client_id_label }}</strong> {{ $receipts[0]->client_id }}
-		@endif
-		@if(!empty($receipts[0]->customer_tax_label))
-			<br/>
-			<strong>{{ $receipts[0]->customer_tax_label }}</strong> {{ $receipts[0]->customer_tax_number }}
-		@endif
-		@if(!empty($receipts[0]->customer_custom_fields))
-			<br/>{!! $receipts[0]->customer_custom_fields !!}
-		@endif
-		@if(!empty($receipts[0]->sales_person_label))
-			<br/>
-			<strong>{{ $receipts[0]->sales_person_label }}</strong> {{ $receipts[0]->sales_person }}
-		@endif
-	</div>
-	<div class="col-md-6 invoice-col width-50 word-wrap">
-		<strong>@lang('lang_v1.shipping_address'):</strong><br>
-		{!! $receipts[0]->shipping_address !!}
-		@if(!empty($receipts[0]->shipping_custom_field_1_label))
-			<br><strong>{!!$receipts[0]->shipping_custom_field_1_label!!} :</strong> {!!$receipts[0]->shipping_custom_field_1_value ?? ''!!}
-		@endif
-
-		@if(!empty($receipts[0]->shipping_custom_field_2_label))
-			<br><strong>{!!$receipts[0]->shipping_custom_field_2_label!!}:</strong> {!!$receipts[0]->shipping_custom_field_2_value ?? ''!!}
-		@endif
-
-		@if(!empty($receipts[0]->shipping_custom_field_3_label))
-			<br><strong>{!!$receipts[0]->shipping_custom_field_3_label!!}:</strong> {!!$receipts[0]->shipping_custom_field_3_value ?? ''!!}
-		@endif
-
-		@if(!empty($receipts[0]->shipping_custom_field_4_label))
-			<br><strong>{!!$receipts[0]->shipping_custom_field_4_label!!}:</strong> {!!$receipts[0]->shipping_custom_field_4_value ?? ''!!}
-		@endif
-
-		@if(!empty($receipts[0]->shipping_custom_field_5_label))
-			<br><strong>{!!$receipts[0]->shipping_custom_field_2_label!!}:</strong> {!!$receipts[0]->shipping_custom_field_5_value ?? ''!!}
-		@endif
-	</div>
-</div>
-
-<div class="row color-555">
-	<div class="col-xs-12">
-		<br/>
-		<table class="table table-bordered table-no-top-cell-border">
-			<thead>
-				<tr style="background-color: #357ca5 !important; color: white !important; font-size: 20px !important" class="table-no-side-cell-border table-no-top-cell-border text-center">
-					<td style="background-color: #357ca5 !important; color: white !important; width: 5% !important">#</td>
-					
-					<td style="background-color: #357ca5 !important; color: white !important; width: 65% !important">
-						{{$receipts[0]->table_product_label}}
-					</td>
-					
-					<td style="background-color: #357ca5 !important; color: white !important; width: 30% !important;">
-						{{$receipts[0]->table_qty_label}}
-					</td>
-				</tr>
-			</thead>
-			<tbody>
-			@php
-			$order = 1;
-			@endphp
-			@foreach($receipts as $receipt_details)
-				@foreach($receipt_details->lines as $line)
-					<tr>
-						<td class="text-center">
-							{{$order++}}
-						</td>
-						<td style="word-break: break-all;">
-                            {{$line['name']}} {{$line['product_variation']}} {{$line['variation']}} 
-                            @if(!empty($line['sub_sku'])), {{$line['sub_sku']}} @endif @if(!empty($line['brand'])), {{$line['brand']}} @endif
-                            @if(!empty($line['product_custom_fields'])), {{$line['product_custom_fields']}} @endif
-                            @if(!empty($line['sell_line_note']))({!!$line['sell_line_note']!!}) @endif
-                            @if(!empty($line['lot_number']))<br> {{$line['lot_number_label']}}:  {{$line['lot_number']}} @endif 
-                            @if(!empty($line['product_expiry'])), {{$line['product_expiry_label']}}:  {{$line['product_expiry']}} @endif 
-                        </td>
-						<td class="text-right">
-							{{$line['quantity']}} {{$line['units']}}
-						</td>
-					</tr>
-					@if(!empty($line['modifiers']))
-						@foreach($line['modifiers'] as $modifier)
-							<tr>
-								<td class="text-center">
-									&nbsp;
-								</td>
-								<td>
-		                            {{$modifier['name']}} {{$modifier['variation']}} 
-		                            @if(!empty($modifier['sub_sku'])), {{$modifier['sub_sku']}} @endif 
-		                            @if(!empty($modifier['sell_line_note']))({!!$modifier['sell_line_note']!!}) @endif 
-		                        </td>
-								<td class="text-right">
-									{{$modifier['quantity']}} {{$modifier['units']}}
-								</td>
-							</tr>
-						@endforeach
-					@endif
-				@endforeach
-			@endforeach
-
-				@php
-				$lines = count($receipts[0]->lines);
-				@endphp
-
-				@for ($i = $lines; $i < 7; $i++)
-    				<tr>
-    					<td>&nbsp;</td>
-    					<td>&nbsp;</td>
-    					<td>&nbsp;</td>
-    				</tr>
-				@endfor
-
-			</tbody>
+	<div style="width: 48%; display:inline-block;padding-left: 61px;">
+		<h2 style="text-align: center; margin-top: 0;font-size: 32px;font-weight: 600;">Rechnung</h2>
+		<table style="font-weight: 600;">
+			<tr>
+				<td>Rechnung-Nr :</td>
+				<td>28414</td>
+			</tr>
+			<tr>
+				<td>Rechnungsdatum :</td>
+				<td>12.10.2022</td>
+			</tr>
+			<tr>
+				<td>Kunden-Nr :</td>
+				<td>14019</td>
+			</tr>
+			<tr>
+				<td>Liefer-/Leistungsdatum :</td>
+				<td>04.10.2022</td>
+			</tr>
+			<tr>
+				<td>UID-Nr/Steuer-Nr :</td>
+				<td></td>
+			</tr>
 		</table>
 	</div>
 </div>
-
-<div class="row invoice-info color-555" style="page-break-inside: avoid !important">
-	<div class="col-md-6 invoice-col width-50">
-		<b class="pull-left">@lang('lang_v1.authorized_signatory')</b>
-	</div>
-</div>
-
-{{-- Barcode --}}
-@if($receipts[0]->show_barcode)
-<br>
-<div class="row">
-		<div class="col-xs-12">
-			<img class="center-block" src="data:image/png;base64,{{DNS1D::getBarcodePNG($receipt_details->invoice_no, 'C128', 2,30,array(39, 48, 54), true)}}">
-		</div>
-</div>
-@endif
-
-@if(!empty($receipts[0]->footer_text))
-	<div class="row color-555">
-		<div class="col-xs-12">
-			{!! $receipts[0]->footer_text !!}
-		</div>
-	</div>
-@endif
-
-			</td>
-		</tr>
+<table style="width: 100%;" cellspacing="0">
+	<thead>
+	<tr style="font-weight: 700;font-size: 16px;">
+		<td style="width: 8%;border-bottom: 1px solid black;">Art.Nr</td>
+		<td style="width: 36%;border-bottom: 1px solid black;">Bezeichnung</td>
+		<td style="width: 8%;border-bottom: 1px solid black;">LND</td>
+		<td style="width: 8%;border-bottom: 1px solid black;">Kolli</td>
+		<td style="width: 8%;border-bottom: 1px solid black;">Inhalt</td>
+		<td style="width: 8%;border-bottom: 1px solid black;">Menge</td>
+		<td style="width: 8%;border-bottom: 1px solid black;">Preis</td>
+		<td style="width: 8%;border-bottom: 1px solid black;">Betrag</td>
+		<td style="width: 8%;border-bottom: 1px solid black;">St.</td>
+	</tr>
+	</thead>
+	<tbody  style="font-weight: 400;font-size: 14px;">
+	<tr>
+		<td></td>
+		<td>-----Lief.Nr.5380; vom 04.10.2022;-----</td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>719</td>
+		<td>Trauben kernlos Liso</td>
+		<td>IT</td>
+		<td>1</td>
+		<td>6,90</td>
+		<td>6,90</td>
+		<td>3,300</td>
+		<td>22,77</td>
+		<td>7%</td>
+	</tr>
+	<tr>
+		<td>719</td>
+		<td>Trauben kernlos Liso</td>
+		<td>IT</td>
+		<td>1</td>
+		<td>6,90</td>
+		<td>6,90</td>
+		<td>3,300</td>
+		<td>22,77</td>
+		<td>7%</td>
+	</tr>
+	<tr>
+		<td>719</td>
+		<td>Trauben kernlos Liso</td>
+		<td>IT</td>
+		<td>1</td>
+		<td>6,90</td>
+		<td>6,90</td>
+		<td>3,300</td>
+		<td>22,77</td>
+		<td>7%</td>
+	</tr>
+	<tr>
+		<td>719</td>
+		<td>Trauben kernlos Liso</td>
+		<td>IT</td>
+		<td>1</td>
+		<td>6,90</td>
+		<td>6,90</td>
+		<td>3,300</td>
+		<td>22,77</td>
+		<td>7%</td>
+	</tr>
+	<tr>
+		<td>719</td>
+		<td>Trauben kernlos Liso</td>
+		<td>IT</td>
+		<td>1</td>
+		<td>6,90</td>
+		<td>6,90</td>
+		<td>3,300</td>
+		<td>22,77</td>
+		<td>7%</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Gurken lang 12er 350/400 BE</td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
 	</tbody>
 </table>
-<style type="text/css">
-	body {
-		color: #000000;
-	}
-</style>
+<p  style="font-weight: 400;font-size: 14px;">Seite 1/3</p>
+<p  style="font-weight: 400;font-size: 12px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quibusdam similique possimus officia vitae praesentium, inventore sequi doloremque, saepe, repellendus molestias unde doloribus! Consequuntur laboriosam velit repudiandae incidunt repellat!</p>
+<table style="font-weight: 600;font-size: 14px;width: 100%;border-top: 1px solid black;padding: 30px;">
+	<tr>
+		<td style="width: 33%;">Tel. +49 761 1374987</td>
+		<td style="width: 33%;">Bankverbindung:</td>
+		<td style="width: 33%;">Sun Früchte Gbr, Günex Mustafa Selcuk</td>
+	</tr>
+	<tr>
+		<td style="width: 33%;">Fax. +49 761 1374988</td>
+		<td style="width: 33%;">Sparkasse Freiburg</td>
+		<td style="width: 33%;">und Güne Volkan Yusuf</td>
+	</tr>
+	<tr>
+		<td style="width: 33%;">Email. sunfruechte@gmail.com</td>
+		<td style="width: 33%;">IBAN: DE89 6805 0101 0014 009705</td>
+		<td style="width: 33%;">USt-IdNr:DE 815848434</td>
+	</tr>
+	<tr>
+		<td style="width: 33%;">Gerichtstand Freiburg</td>
+		<td style="width: 33%;">BIC: FRSPDFE66XXX</td>
+		<td style="width: 33%;">Steuer-Nr: 06346/43383</td>
+	</tr>
+</table>
+<table style="font-weight: 600;font-size: 16px;width: 100%;">
+	<tr>
+		<td style="width: 40%;">Netto Warenbetrag(7%)</td>
+		<td style="width: 10%;">90,48</td>
+		<td style="width: 40%;">MwSt(7%)</td>
+		<td style="width: 10%;text-align: right;">6,37</td>
+	</tr>
+	<tr>
+		<td>Netto Warenbetrag(19%)</td>
+		<td>-34,48</td>
+		<td>MwSt(19%)</td>
+		<td style="width: 10%;text-align: right;">-6,55</td>
+	</tr>
+</table>
+<table style="width: 100%; padding-left: 60px;">
+	<tr>
+		<td style="width: 15%;font-weight: 400;font-size: 14px;">4 Artikel</td>
+		<td style="width: 15%;font-weight: 400;font-size: 14px;">5 Kolli</td>
+		<td style="width: 50%;font-weight: 600;font-size: 20px;">Lieferscheinbetrag</td>
+		<td style="width: 20%;font-weight: 600;font-size: 20px;text-align: right;">56.32</td>
+	</tr>
+</table>
+</body>
+</html>
